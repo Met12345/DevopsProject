bdec0ca3b47929fe96d13a8b71b4ef887f4552ba
Met12345 <89018941+Met12345@users.noreply.github.com>
2025-09-05 17:01:27 +0530

    Update vpc.tf

--- terraform_code/eks_code/vpc.tf
+++ terraform_code/eks_code/vpc.tf
@@ -1,22 +1,27 @@
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.13.0"
+  version = "~> 5.1"
 
-  name = local.name
-  cidr = local.vpc_cidr
+  name            = local.name
+  cidr            = local.vpc_cidr
 
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
   intra_subnets   = local.intra_subnets
 
-  enable_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_support     = true
+  enable_dns_hostnames   = true
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
+    "kubernetes.io/role/elb" = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
+    "kubernetes.io/role/internal-elb" = "1"
   }
+
+  tags = local.tags
 }
